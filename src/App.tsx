e467db69db4949472b8866c28bf034514d586e46
Markevo5150 <marcoAlopez2@outlook.com>
2023-10-21 15:14:54 -0600

    First files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="whitePart">
+          <h1>Join our community</h1>
+        </div>
+        <div className="greenPart"></div>
+        <div className="softGreenPart"></div>
+      </div>
+    </>
+  );
+}
