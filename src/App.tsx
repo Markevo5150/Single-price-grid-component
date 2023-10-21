548eddb0695140025b0b91d6476c21d32c76120b
Markevo5150 <marcoAlopez2@outlook.com>
2023-10-21 15:23:05 -0600

    Avances

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,23 @@ export default function App() {
     <>
       <div className="main-container">
         <div className="whitePart">
-          <h1>Join our community</h1>
+          <h1 className="title">Join our community</h1>
+          <h2 className="promo">30-day. hassle-free money back guarantee</h2>
+          <p className="description">
+            Gain access to our full library of tutorials along with expert code
+            reviews. Perfect for any developers who are serious about honing
+            their skills.
+          </p>
         </div>
-        <div className="greenPart"></div>
+        <div className="greenPart">
+          <h2 className="subscriptionTitle">Monthly Subscription</h2>
+          <div className="prices">
+            <span className="actPrice">$29</span>
+            <span className="time">per month</span>
+          </div>
+          <p className="accesInfo">Full access for less than $1 a day</p>
+          <button>Sign Up</button>
+        </div>
         <div className="softGreenPart"></div>
       </div>
     </>
